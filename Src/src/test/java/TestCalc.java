3d00c1df0067b9de3f2d4d789d56f1245af4bc0f
Jrod <rod21060@uvg.edu.gt>
2024-07-04 12:58:03 -0600

    Pruebas unitaria

--- Src/src/test/java/TestCalc.java
+++ Src/src/test/java/TestCalc.java
@@ -1,2 +1,30 @@
-public class TestCalc {
+public class CalcTest {
+
+    private Calc calc;
+
+    
+    public void setUp() {
+        calc = new Calc();
+    }
+
+    
+    public void testSuma() {
+        assertEquals(5, calc.suma(2, 3));
+        assertEquals(0, calc.suma(-1, 1));
+        assertEquals(-5, calc.suma(-3, -2));
+    }
+
+    
+    public void testResta() {
+        assertEquals(1, calc.resta(3, 2));
+        assertEquals(-2, calc.resta(-1, 1));
+        assertEquals(-1, calc.resta(-3, -2));
+    }
+
+    
+    public void testMultiplicacion() {
+        assertEquals(6, calc.multiplicacion(2, 3));
+        assertEquals(-1, calc.multiplicacion(-1, 1));
+        assertEquals(6, calc.multiplicacion(-3, -2));
+    }
 }
