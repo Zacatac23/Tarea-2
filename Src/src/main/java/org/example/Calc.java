71c93b198bcb1f02ef165e472f2f4f3e8488d738
Emadlgg <osmanemanuel2004@gmail.com>
2024-07-04 12:12:41 -0600

    Clase Calc realizada

--- Src/src/main/java/org/example/Calc.java
+++ Src/src/main/java/org/example/Calc.java
@@ -1,4 +1,19 @@
 package org.example;
 
 public class Calc {
+
+    // Método para sumar dos números
+    public int suma(int n1, int n2) {
+        return n1 + n2;
+    }
+
+    // Método para restar dos números
+    public int resta(int n1, int n2) {
+        return n1 - n2;
+    }
+
+    // Método para multiplicar dos números
+    public int multiplicacion(int n1, int n2) {
+        return n1 * n2;
+    }
 }
